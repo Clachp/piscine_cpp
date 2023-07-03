12466b390834515e69b42d4d70843930e61520f5
Claire Chapon <claire_1105@hotmail.fr>
2023-07-04 00:32:10 +0200

    module08 ex 03

--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -3,24 +3,32 @@
 
 #include <iostream>
 #include <exception>
+#include <algorithm>
 #include <vector>
 #include <deque>
 #include <list>
 
+class notFoundException : public std::exception {
+	public :
+		virtual const char* what() const throw () {
+			return "Occcurence not found exception\n";
+		}
+};
+
 template<typename T>
 void easyfind (T const & target, int n) {
 	try {
-		for (size_t i = 0; i < target.size(); i++) {
-			if (target[i] == n) {
-				std::cout << "value " << n << " is at index " << i << std::endl;
-				return ; 
-			}
-			throw std::exception();
-		}
+		typename T::const_iterator it;
+		it = std::find (target.begin(), target.end(), n);
+  		if (it != target.end())
+			std::cout << "value " << n << " is at index " << *it << std::endl;
+  		else
+			throw notFoundException();
 	}
 	catch (std:: exception &e){
 		std::cerr << "Error\n" << "value " << n << " not found\n";
 		std::cerr << e.what() << std::endl; 
 	}
-}
+};
+
 #endif
--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -2,9 +2,23 @@
 
 int main (void)
 {
-	std::vector<int> tab(8, 40);
-	tab.assign(4, 20);
-	easyfind(tab, 20);
+	// vector : ~tableau
+	std::vector<int> tab(4, 42);
+	for (int i = 1; i < 8; i++)
+		tab.push_back(i + 5);
+	 std::cout << "vector tab contains:";
+	for (std::vector<int>::iterator it = tab.begin() ; it != tab.end(); ++it)
+		std::cout << ' ' << *it;
+	std::cout << '\n';
+	easyfind(tab, -40);
+
+	//list : ~liste doublement chainée 
+	std::list<int>lst(5, 100);
+	easyfind(lst, 100);
+
+	//deque
+	std::deque<int>deq = [5, 78, 40, -4, 0]; // declarer deque
+	easyfind(tab, 0);
 
 	return 0;
 }
